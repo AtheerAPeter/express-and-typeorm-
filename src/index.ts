35f98a4b5d691fa2d6bdb2ccc8afabfa44c86c30
atheer <atheer@pop-os.localdomain>
2020-10-22 20:10:59 +0300

    fixing db

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { Product } from "./entity/Product";
 createConnection()
   .then(async (connection) => {
     const app = express();
-    const port = 3000;
+    const port = process.env.PORT || 3000;
 
     app.use(express.json());
 
